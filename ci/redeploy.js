30b67ad11c0791d9cc76debf91f317f98fe3afee
Josh Farrant <josh@farrant.me>
2018-03-15 16:37:42 +0000

    Testing ci

--- ci/redeploy.js
+++ ci/redeploy.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-console */
+
 const http = require('http');
+const { exec } = require('child_process');
 const createHandler = require('github-webhook-handler');
 
 const handler = createHandler({ path: '/webhook', secret: process.env.GITHUB_SECRET });
@@ -12,9 +15,18 @@ http.createServer((req, res) => {
 }).listen(PORT);
 
 handler.on('error', (err) => {
-  console.error('Error:', err.message); // eslint-disable-line no-console
+  console.error('Error:', err.message);
 });
 
 handler.on('push', () => {
-  console.log('Push'); // eslint-disable-line no-console
+  console.log('Push');
+
+  exec('cd /var/www/farrant-me && git pull && npm i && npm run build', (err, stdout, stderr) => {
+    if (err) {
+      return;
+    }
+
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
 });
